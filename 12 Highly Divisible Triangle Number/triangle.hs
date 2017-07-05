3f033e3553716de84123de944547f2a6ad430af9
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-07-05 09:13:23 +0530

    Made something that should be a lot more reusable for problem 12

--- 12 Highly Divisible Triangle Number/triangle.hs
+++ 12 Highly Divisible Triangle Number/triangle.hs
@@ -6,9 +6,13 @@ divisible x y
 triangle :: [Int]
 triangle = [n * (n-1) `quot` 2 | n <- [2..]]
 
-numDivisors :: Int -> Int
-numDivisors x = 2 * (length $ filter (divisible x) [1..sqrtX])
+divisors :: Int -> [Int]
+divisors x = (smallDivisors) ++ (reverse $ map (\i -> x `quot` i) $ smallDivisors)
     where sqrtX = floor $ sqrt $ fromIntegral $ x
+          smallDivisors = filter (divisible x) [1..sqrtX]
+
+numDivisors :: Int -> Int
+numDivisors x = length $ divisors x
 
 main = do
     print $ head $ filter (\x -> (numDivisors x) > 500) triangle
